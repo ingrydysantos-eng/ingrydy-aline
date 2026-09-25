34ca62b9cb31cc4a2b85d8a2f054836e70e77af5
ingrydysantos-eng <ingrydy.santos@escola.pr.gov.br>
2026-09-25 09:09:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,45 +6,45 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
- enunciado: "Pergunta 1" ,
+ enunciado: "Vôce acha que a sociedade deve ajudar pessoas em situação de pobreza?" ,
    alternativas: [
-    "Alternativa 1" 
-     "Alternativa 2"
-    ]
+    "Sim, porque todos devem ter oportunidades de acesso a condição básicas de vida" 
+     "Não porque cada pessoa deve ser responsável por buscar suas próprias condições de vida"]
 },
 { 
 
-        enunciado: "Pergunta 1" ,
+        enunciado: "A desigualdade social social é um problema que a feta a sociedade" ,
           alternativas: [
-           "Alternativa 1" 
-            "Alternativa 2"
+           "sim, porque a desigualdade social dificuldade sociak dificuklta a acesso de muitas pessoas tenham oportunidades de melhorar suas condições de vida" 
+            "Não, pois a desigualdade social não impede que todas as pessoas tenham oportunidades de melhorar suas condições  de vida"
            ]
        },
-    },
-    { 
-    
-            enunciado: "Pergunta 1" ,
+       {
+            enunciado: "A falta de emprego poe aumentar os problemas sociais" ,
               alternativas: [
-               "Alternativa 1" 
-                "Alternativa 2"
+               "Sim,por falta de emprego pode aumentar a problema e dificultar o acesso das familias a necessidade básicas" 
+                "Não, porque a falta de emprego não determina a condição social de uma pessoa, pois existem outras formas de obter renda e apoio"
                ]
-           },
-        },
-        { 
-        
-                enunciado: "Pergunta 1" ,
+              },
+            {
+                enunciado: "A pobreza influencia a qualidadede vida das pessoas",
                   alternativas: [
-                   "Alternativa 1" 
-                    "Alternativa 2"
+                   "sim, a pobreza pode dificultar o acesso á alimentação, moradia, saúde e educação" 
+                    "Não, a pobreza não impede completamente o acesso aos serviços básicos e ás oportunidades de malhoria de vida"
                    ]
+                 
                },
-];
-let atual =0;
-let  perguntaAtual;
-function  mostraPergunta(){
+                       ];
 
-}
+     let atual =0;
+    let  perguntaAtual;
+   
+    function  mostraPergunta(){
+perguntaAtual = Perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+} 
 
+mostraPergunta();
 
 
 
